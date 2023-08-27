7806ea003e3c84f2e1c99e659a252636813a71ef
agpavlik <agpavlik@gmail.com>
2023-08-27 15:17:02 -0400

    Add style based on css modules

---
+++ src/components/AppNav.jsx
@@ -0,0 +1,7 @@
+import styles from "./AppNav.module.css"; //could be destructured from styles to {nav}
+
+function AppNav() {
+  return <nav className={styles.nav}>App Navigation</nav>;
+}
+
+export default AppNav;
---
+++ src/pages/AppLayout.jsx
@@ -0,0 +1,14 @@
+import AppNav from "../components/AppNav";
+
+function AppLayout() {
+  return (
+    <div>
+      <h1>
+        <AppNav />
+        <p>App</p>
+      </h1>
+    </div>
+  );
+}
+
+export default AppLayout;
