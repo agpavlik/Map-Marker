6e8df2520db02a82bed1228cc0545456255b6a8f
agpavlik <agpavlik@gmail.com>
2023-08-27 00:33:22 -0400

    Add basic pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Product from "./pages/Product";
+import Pricing from "./pages/Pricing";
+import Homepage from "./pages/Homepage";
+import PageNotFound from "./pages/PageNotFound";
+// import PageNav from "../components/PageNav";
+
 function App() {
-  return <div>WorldWise</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="product" element={<Product />} />
+        <Route path="pricing" element={<Pricing />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
