a77872df7aad260edb002f39ac3cb4038363a1c7
agpavlik <agpavlik@gmail.com>
2023-09-08 21:12:35 -0400

    Implement fetching city data in the Form

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -14,19 +14,19 @@ import {
 import { useCities } from "../contexts/CitiesContext";
 import { useGeolocation } from "../hooks/useGeolocation";
 import Button from "./Button";
+import { useUrlPosition } from "../hooks/useUrlPosition";
 
 function Map() {
   const { cities } = useCities();
   const [mapPosition, setMapPosition] = useState([40, 0]);
-  const [searchParams] = useSearchParams();
+
   const {
     isLoading: isLoadingPosition,
     position: geolocationPosition,
     getPosition,
   } = useGeolocation();
 
-  const mapLat = searchParams.get("lat");
-  const mapLng = searchParams.get("lng");
+  const [mapLat, mapLng] = useUrlPosition();
 
   useEffect(
     function () {
@@ -89,7 +89,7 @@ function ChangeCenter({ position }) {
 function DetectClick() {
   const navigate = useNavigate();
   useMapEvents({
-    click: (e) => navigate("form? lat=${e.latlng.lat}& lng={e.latlng.lng}"),
+    click: (e) => navigate(`form?lat=${e.latlng.lat}&lng=${e.latlng.lng}`),
   });
 }
 
--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -4,7 +4,7 @@ import styles from "./Message.module.css";
 function Message({ message }) {
   return (
     <p className={styles.message}>
-      <span role="img">🏙️</span> {message}
+      <span role="img">🏛️</span> {message}
     </p>
   );
 }
